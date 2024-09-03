dc805e1ef4a39dceed2e87bcb6dc6841a5d3c82a
jamnny <164428308+jamnny@users.noreply.github.com>
2024-09-04 00:34:47 +0800

    updated for cp

--- src/sects/Projects/Proj.jsx
+++ src/sects/Projects/Proj.jsx
@@ -15,36 +15,36 @@ function Proj() {
             <div className={styles.projectsContainer}>
                 <a href="https://github.com/" target="_blank" className="hover">
                     <img className={styles.dd} src={clinic} alt="Clinic logo" />
-                    <h3>Web Responsive Application</h3>
+                    <h3 className={styles.h3}>Web Responsive Application</h3>
                     <p className={styles.des}>Web Responsive Tele-Counseling Application</p>
                 </a>
                 <a href="https://github.com/" target="_blank" className="hover">
                     <img className={styles.dd} src={vidcall} alt="VideoCall logo" />
-                    <h3>Video Call</h3>
+                    <h3 className={styles.h3}>Video Call Web Application</h3>
                     <p className={styles.des}>Video call using agora, Laravel, php, MySQL, html, CSS, java script</p>
                 </a>
             </div>
             <div className={styles.projectsContainer}>
                 <a href="https://github.com/" target="_blank" className="hover">
                     <img className={styles.dd} src={pay} alt="Payments logo" />
-                    <h3>Online Payment</h3>
+                    <h3 className={styles.h3}>Online Payment</h3>
                     <p className={styles.des}>Online Payment using paymongo, Laravel, php, MySQL, html, CSS, java script</p>
                 </a>
                 <a href="https://github.com/" target="_blank" className="hover">
                     <img className={styles.dd} src={lend} alt="Lending logo" />
-                    <h3>Lending System Application</h3>
+                    <h3 className={styles.h3}>Lending System Application</h3>
                     <p className={styles.des}>First application I made using java</p>
                 </a>
             </div>
             <div className={styles.projectsContainer}>
                 <a href="https://github.com/" target="_blank" className="hover">
                     <img className={styles.dd} src={dart} alt="Reminder logo" />
-                    <h3>Reminder Application</h3>
+                    <h3 className={styles.h3}>Reminder Application</h3>
                     <p className={styles.des}>First android application I made using dart, flutter, and android studio</p>
                 </a>
                 <a href="https://github.com/" target="_blank" className="hover">
                     <img className={styles.dd} src={basic} alt="BasicLending logo" />
-                    <h3>Basic Recording System</h3>
+                    <h3 className={styles.h3}>Basic Recording System</h3>
                     <p className={styles.des}>Basic web app using php, MySQL, html, CSS</p>
                 </a>
             </div>
